a78aa08e95da9ad1ac2d5f1e2fde8439ca1ce179
XYFrain <xiaofeng132@hotmail.com>
2026-07-10 23:43:49 +0800

    实现单实例运行，只允许启动一个应用

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 
 namespace TWlauncher
 {
-    /// <summary>
-    /// App.xaml 的交互逻辑
-    /// </summary>
     public partial class App : Application
     {
+        private Mutex _mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // 程序唯一标识
+            string mutexName = "TWLauncher_SingleInstance_Mutex";
+            bool createNew;
+
+            // 创建互斥体
+            _mutex = new Mutex(true, mutexName, out createNew);
+
+            if (!createNew)
+            {
+                _mutex = null;
+                MessageBox.Show("启动器已在运行，请勿重复打开！");
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // 释放互斥锁
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Dispose();
+                _mutex = null;
+            }
+            base.OnExit(e);
+        }
     }
 }
